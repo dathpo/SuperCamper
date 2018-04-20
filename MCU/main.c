f2c0a8887baf4a9b97329714a0e32611273a2afe
David T. Pocock <david.pocock.2013@uni.strath.ac.uk>
2018-04-20 16:16:55 +0100

    Improvements on MSP430 WiFi setup, still WOP

--- MCU/main.c
+++ MCU/main.c
@@ -18,7 +18,8 @@ unsigned int PWM_Period     = (MCU_CLOCK / PWM_FREQUENCY);  // PWM Period
 unsigned int PWM_Duty       = 0;                            // %
 unsigned int MOTOR_PWM_Duty = 14000;
 
-
+int index_tx=0,index_rx=0;
+char Reply[];
 //Server.
 //const char * Server[]={
 //"AT+CWMODE=3\r\n",
@@ -29,16 +30,37 @@ unsigned int MOTOR_PWM_Duty = 14000;
 
 //WiFi access point.
 const char * AP[]={
-		//"Z\r\n",
-		"AT+RST\r\n",
-		"AT+CWMODE=3\r\n",
-		"AT+CWSAP=\"Test\",\"password\",3,0\r\n",
-		"AT+CIFSR\r\n",
-		"AT+CIPMUX=1\r\n",
-		"AT+CIPSERVER=1,100\r\n",
-		"AT+SLEEP=0\r\n",
+                   //"Z\r\n",
+                   "AT+RST\r\n",
+                   "AT+CWMODE=3\r\n",
+                   "AT+CWSAP=\"Ciao\",\"password\",3,0\r\n",
+                   "AT+CIFSR\r\n",
+                   "AT+CIPMUX=1\r\n",
+                   "AT+CIPSERVER=1,100\r\n",
+                   "AT+SLEEP=0\r\n",
 };
 
+void WiFiSetup() {
+    int size= sizeof(AP)/sizeof(AP[0]);
+    P1OUT |= TXLED;
+    int i=-1;
+    for (index_tx=0;index_tx<size;index_tx++) {
+        while(AP[index_tx][i++]!='\n')
+        {
+            char tmp = AP[index_tx][i];
+            UCA0TXBUF = AP[index_tx][i];
+        }
+        __delay_cycles(800000);
+        i=-1;
+    }
+    //UCA0TXBUF = AP[index][i++]; // TX next character
+    /* if (i == strlen(AP[index])+1) // TX over?
+    {
+        //UC0IE &= ~UCA0TXIE;
+        UC0IE |= UCA0RXIE;
+    } */
+    P1OUT &= ~TXLED;
+}
 
 //Creates TCP server. Port number is 333 by default.
 //const char test[] = {"AT+CIPSERVER=1,100\r\n"};
@@ -65,35 +87,33 @@ int main(void)
 
     __enable_interrupt();
 
-	unsigned int servo_stepval, servo_stepnow;
-	unsigned int servo_lut[ SERVO_STEPS+1 ];
-	unsigned int position;
-	size= sizeof(AP)/sizeof(AP[0]);
+    unsigned int servo_stepval, servo_stepnow;
+    unsigned int servo_lut[ SERVO_STEPS+1 ];
+    unsigned int position;
+    // Calculate the step value and define the current step, defaults to minimum.
+    servo_stepval   = ( (SERVO_MAX - SERVO_MIN) / SERVO_STEPS );
+    servo_stepnow   = SERVO_MIN;
 
-	// Calculate the step value and define the current step, defaults to minimum.
-	servo_stepval   = ( (SERVO_MAX - SERVO_MIN) / SERVO_STEPS );
-	servo_stepnow   = SERVO_MIN;
+    // Fill up the LUT
+    for (position = 0; position < SERVO_STEPS; position++) {
+        servo_stepnow += servo_stepval;
+        servo_lut[position] = servo_stepnow;
+    }
 
-	// Fill up the LUT
-	for (position = 0; position < SERVO_STEPS; position++) {
-		servo_stepnow += servo_stepval;
-		servo_lut[position] = servo_stepnow;
-	}
+    // Setup the PWM, etc.
+    TACCTL1 = OUTMOD_7;            // TACCR1 reset/set
+    TACTL   = TASSEL_2 + MC_1;     // SMCLK, upmode
+    TACCR0  = PWM_Period-1;        // PWM Period
+    TACCR1  = PWM_Duty;            // TACCR1 PWM Duty Cycle
+    P1DIR   |= BIT6;               // P2.6 = output
+    P1SEL   |= BIT6;               // P2.6 = TA1 output
 
-	// Setup the PWM, etc.
-	TACCTL1 = OUTMOD_7;            // TACCR1 reset/set
-	TACTL   = TASSEL_2 + MC_1;     // SMCLK, upmode
-	TACCR0  = PWM_Period-1;        // PWM Period
-	TACCR1  = PWM_Duty;            // TACCR1 PWM Duty Cycle
-	P1DIR   |= BIT6;               // P2.6 = output
-	P1SEL   |= BIT6;               // P2.6 = TA1 output
-
-	// P1.3 Button Interrupt Config
-	P1IE |=  BIT3;                            // P1.3 interrupt enabled
+    // P1.3 Button Interrupt Config
+    P1IE |=  BIT3;                            // P1.3 interrupt enabled
     P1IES |= BIT3;                            // P1.3 Hi/lo edge
     P1REN |= BIT3;                            // Enable Pull Up on SW2 (P1.3)
     P1IFG &= ~BIT3;                           // P1.3 IFG cleared
-                                              //BIT3 on Port 1 can be used as Switch
+    //BIT3 on Port 1 can be used as Switch
     // Motor Control Setup
     TACCTL2 = OUTMOD_7;            // TACCR1 reset/set
     TACCR2  = MOTOR_PWM_Duty;            // TACCR1 PWM Duty Cycle
@@ -102,107 +122,110 @@ int main(void)
 
     P1DIR |= BIT5;
 
-	// WiFi Setup
-	DCOCTL = 0; 			 // Select lowest DCOx and MODx settings
-	BCSCTL1 = CALBC1_1MHZ;   // Set DCO
-	DCOCTL = CALDCO_1MHZ;
-	P2DIR |= 0xFF;			 // All P2.x outputs
-	P2OUT &= 0x00; 			 // All P2.x reset
-	P1SEL |= RXD + TXD ;     // P1.1 = RXD, P1.2=TXD
-	P1SEL2 |= RXD + TXD ;    // P1.1 = RXD, P1.2=TXD
-	P1DIR |= TXLED;
-	//P1OUT &= 0x00;
-	UCA0CTL1 |= UCSSEL_2;        // SMCLK
-	UCA0BR0 = 0x08;              // 1MHz 115200
-	UCA0BR1 = 0x00;              // 1MHz 115200
-	UCA0MCTL = UCBRS2 + UCBRS0;  // Modulation UCBRSx = 5
-	UCA0CTL1 &= ~UCSWRST;        // **Initialize USCI state machine**
-	UC0IE |= UCA0TXIE;          // Enable USCI_A0 TX interrupt
-	UC0IE &= ~UCA0RXIE;         // Disable RX.
+    // WiFi Setup
+    DCOCTL = 0; 			 // Select lowest DCOx and MODx settings
+    BCSCTL1 = CALBC1_1MHZ;   // Set DCO
+    DCOCTL = CALDCO_1MHZ;
+    P2DIR |= 0xFF;			 // All P2.x outputs
+    P2OUT &= 0x00; 			 // All P2.x reset
+    P1SEL |= RXD + TXD ;     // P1.1 = RXD, P1.2=TXD
+    P1SEL2 |= RXD + TXD ;    // P1.1 = RXD, P1.2=TXD
+    P1DIR |= TXLED;
+    //P1OUT &= 0x00;
+    UCA0CTL1 |= UCSSEL_2;        // SMCLK
+    UCA0BR0 = 0x08;              // 1MHz 115200
+    UCA0BR1 = 0x00;              // 1MHz 115200
+    UCA0MCTL = UCBRS2 + UCBRS0;  // Modulation UCBRSx = 5
+    UCA0CTL1 &= ~UCSWRST;        // **Initialize USCI state machine**
+    //UC0IE |= UCA0TXIE;          // Enable USCI_A0 TX interrupt
+    UC0IE &= ~UCA0RXIE;         // Disable RX.
+    WiFiSetup();
+    UC0IE |= UCA0RXIE;
 
-	position=95;
+    position=95;
 
-	//Center is position 95.
-	TACCR1 = servo_lut[position];
-	__delay_cycles(20000);
+    //Center is position 95.
+    TACCR1 = servo_lut[position];
+    __delay_cycles(20000);
 
-	//Comment this line out for servo use.
-	__bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+    //Comment this line out for servo use.
+    __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
 
-	//Max range is 117 (right side).
-	//Min range is 78. (left side).
-	while (1) {
-		if(flag_right||flag_left){
-			// Move forward toward the maximum step value
-			if(flag_right&&flag_right<117){
-				position=position+5;
-				TACCR1 = servo_lut[position];
-				__delay_cycles(20000);
-			}
-			// Move backward toward the minimum step value
-			if(flag_left&&flag_left>78){
-				position--;
-				TACCR1 = servo_lut[position];
-				__delay_cycles(20000);
-			}
-			UC0IE &= ~UCA0TXIE;
-			//UC0IE |= UCA0RXIE;
-			flag_right = 0;
-			flag_left = 0;
-			__bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
-		}
-		else if(flag_center){
-			position=95;
-			TACCR1 = servo_lut[position];
-			__delay_cycles(20000);
-			flag_center=0;
-			UC0IE &= ~UCA0TXIE;
-			__bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
-		}
-	}
+    //Max range is 117 (right side).
+    //Min range is 78. (left side).
+    while (1) {
+        if(flag_right||flag_left){
+            // Move forward toward the maximum step value
+            if(flag_right&&flag_right<117){
+                position=position+5;
+                TACCR1 = servo_lut[position];
+                __delay_cycles(20000);
+            }
+            // Move backward toward the minimum step value
+            if(flag_left&&flag_left>78){
+                position--;
+                TACCR1 = servo_lut[position];
+                __delay_cycles(20000);
+            }
+            //UC0IE &= ~UCA0TXIE;
+            //UC0IE |= UCA0RXIE;
+            flag_right = 0;
+            flag_left = 0;
+            __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+        }
+        else if(flag_center){
+            position=95;
+            TACCR1 = servo_lut[position];
+            __delay_cycles(20000);
+            flag_center=0;
+            //UC0IE &= ~UCA0TXIE;
+            __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+        }
+    }
 }
 
-#pragma vector=USCIAB0TX_VECTOR
+/*#pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void)
 {
-	P1OUT |= TXLED;
-	UCA0TXBUF = AP[index][i++]; // TX next character
-	if (i == strlen(AP[index])+1) // TX over?
-	{
-		UC0IE &= ~UCA0TXIE;
-		UC0IE |= UCA0RXIE;
-	}
-	P1OUT &= ~TXLED;
-}
+    P1OUT |= TXLED;
+    UCA0TXBUF = AP[index][i++]; // TX next character
+    if (i == strlen(AP[index])+1) // TX over?
+    {
+        UC0IE &= ~UCA0TXIE;
+        UC0IE |= UCA0RXIE;
+    }
+    P1OUT &= ~TXLED;
+} */
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-	//P1OUT |= RXLED;
-	index++;
+    //P1OUT |= RXLED;
 
-	if(index>=size){
-		UC0IE &= ~UCA0TXIE; // Enable USCI_A0 TX interrupt
-	}
-	else
-	{
-		UC0IE |= UCA0TXIE;
-		UC0IE &= ~UCA0RXIE;
-		i=0;
-	}
-	if(UCA0RXBUF=='Q'){
-		flag_right = 1;
-	}
-	if(UCA0RXBUF=='Z'){
-		flag_left = 1;
-	}
-	if(UCA0RXBUF=='K')
-	{
-		flag_center = 1;
-	}
-	if(flag_right||flag_left||flag_center){
-		__bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
-	}
+    if(index>=size){
+        UC0IE &= ~UCA0TXIE; // Enable USCI_A0 TX interrupt
+    }
+    else
+    {
+        UC0IE |= UCA0TXIE;
+        UC0IE &= ~UCA0RXIE;
+        i=0;
+    }
+    Reply[index_rx++]= UCA0RXBUF;
+
+    if(UCA0RXBUF=='Q'){
+        flag_right = 1;
+    }
+    if(UCA0RXBUF=='Z'){
+        flag_left = 1;
+    }
+    if(UCA0RXBUF=='K')
+    {
+        flag_center = 1;
+    }
+    if(flag_right||flag_left||flag_center){
+        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+    }
 }
 //Toggle LED.
 //P1OUT &= ~RXLED;
@@ -211,6 +234,6 @@ __interrupt void USCI0RX_ISR(void)
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-  P1OUT ^= BIT5;                            // P1.0 = toggle
-  P1IFG &= ~BIT3;                           // P1.3 IFG cleared
+    P1OUT ^= BIT5;                            // P1.0 = toggle
+    P1IFG &= ~BIT3;                           // P1.3 IFG cleared
 }
