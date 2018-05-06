017c21e30812672c6ca1740430a13b258dc1d26b
U-DESKTOP-TD68BQK\Paolo <lazzari.paolok@gmail.com>
2018-05-06 19:07:01 +0100

    Added some comments, almost ready for report

--- MCU/main.c
+++ MCU/main.c
@@ -17,21 +17,29 @@
 #define SERVO_MAX           2700    // The maximum duty cycle
 
 unsigned int PWM_Period     = (MCU_CLOCK / PWM_FREQUENCY);  // PWM Period
-unsigned int PWM_Duty       = 0;                            // %
+unsigned int PWM_Duty       = 0;
 unsigned int MOTOR_PWM_Duty = 0;
 
 char temp[50];
-//WiFi access point.
+
+//AT commands used for ESP8266 configuration.
 const char * AP[]={
+        //Reset ESP8266 module.
         "AT+RST\r\n",
+        //Configure ESP8266 module as SoftAP.
         "AT+CWMODE_CUR=2\r\n",
+        //Configure access point.
         "AT+CWSAP_CUR=\"BTOpenzone\",\"Ciao1234567\",3,3\r\n",
+        //Enable multiple connections.
         "AT+CIPMUX=1\r\n",
+        //Setup TCP server on port 100.
         "AT+CIPSERVER=1,100\r\n",
+        //Disable TCP server timeout.
         "AT+CIPSTO=0\r\n",
 };
 
-unsigned int i;     //Counter
+//Variables declaration.
+unsigned int i;
 int index=0;
 int size;
 int flag_left = 0;
@@ -45,7 +53,7 @@ int previous = 0;
 
 int main(void)
 {
-    WDTCTL  = WDTPW + WDTHOLD;     // Kill watchdog timer
+    WDTCTL  = WDTPW + WDTHOLD; // Kill watchdog timer
 
     __enable_interrupt();
 
@@ -53,107 +61,108 @@ int main(void)
     unsigned int servo_lut[ SERVO_STEPS+1 ];
     unsigned int position;
     size= sizeof(AP)/sizeof(AP[0]);
-    BCSCTL3 |= LFXT1S_2;    //initialise clock registers
+    BCSCTL3 |= LFXT1S_2; //Initialise clock registers
 
-    // Calculate the step value and define the current step, defaults to minimum.
-    servo_stepval   = ( (SERVO_MAX - SERVO_MIN) / SERVO_STEPS );
+    servo_stepval   = ( (SERVO_MAX - SERVO_MIN) / SERVO_STEPS ); //Calculate step value
     servo_stepnow   = SERVO_MIN;
 
-    // Fill up the LUT
-    for (position = 0; position < SERVO_STEPS; position++) {
+    for (position = 0; position < SERVO_STEPS; position++) { //Fill look up table with positions
         servo_stepnow += servo_stepval;
         servo_lut[position] = servo_stepnow;
     }
+    //Max value of is 117 (right side).
+    //Min range is 78 (left side).
 
-    // Setup the PWM, etc.
-    TACCTL1 = OUTMOD_7;            // TACCR1 reset/set
-    TACTL   = TASSEL_2 + MC_1;     // SMCLK, upmode
-    TACCR0  = PWM_Period-1;        // PWM Period
-    TACCR1  = PWM_Duty;            // TACCR1 PWM Duty Cycle
-    P1DIR   |= BIT6;               // P2.6 = output
-    P1SEL   |= BIT6;               // P2.6 = TA1 output
+    //PWM setup.
+    TACCTL1 = OUTMOD_7;               // TACCR1 reset/set
+    TACTL   = TASSEL_2 + MC_1;        // SMCLK, upmode
+    TACCR0  = PWM_Period-1;           // PWM Period
+    TACCR1  = PWM_Duty;               // TACCR1 PWM Duty Cycle
+    P1DIR   |= BIT6;                  // P2.6 = output
+    P1SEL   |= BIT6;                  // P2.6 = TA1 output
 
-    // Motor Control Setup
-    TACCTL2 = OUTMOD_7;            // TACCR1 reset/set
-    TACCR2  = MOTOR_PWM_Duty;            // TACCR1 PWM Duty Cycle
-    P1DIR |= BIT4;                            // Set P1.0 to output direction
+    //Motor Control Setup
+    TACCTL2 = OUTMOD_7;               // TACCR1 reset/set
+    TACCR2  = MOTOR_PWM_Duty;         // TACCR1 PWM Duty Cycle
+    P1DIR |= BIT4;                    // Set P1.0 to output direction
     P1SEL &= ~BIT4;
     P1OUT &= ~BIT4;
     P1DIR |= BIT5;
 
     // WiFi Setup
-    DCOCTL = 0;              // Select lowest DCOx and MODx settings
-    BCSCTL1 = CALBC1_1MHZ;   // Set DCO
+    DCOCTL = 0;                       // Select lowest DCOx and MODx settings
+    BCSCTL1 = CALBC1_1MHZ;            // Set DCO
     DCOCTL = CALDCO_1MHZ;
-    P2DIR |= 0xFF;           // All P2.x outputs
-    P2OUT &= 0x00;           // All P2.x reset
-    P1SEL |= RXD + TXD ;     // P1.1 = RXD, P1.2=TXD
-    P1SEL2 |= RXD + TXD ;    // P1.1 = RXD, P1.2=TXD
+    P2DIR |= 0xFF;                    // All P2.x outputs
+    P2OUT &= 0x00;                    // All P2.x reset
+    P1SEL |= RXD + TXD ;              // P1.1 = RXD, P1.2=TXD
+    P1SEL2 |= RXD + TXD ;             // P1.1 = RXD, P1.2=TXD
     P1DIR |= TXLED;
     P1OUT &= ~TXLED;
 
-    UCA0CTL1 |= UCSSEL_2;        // SMCLK
-    UCA0BR0 = 0x08;              // 1MHz 115200
-    UCA0BR1 = 0x00;              // 1MHz 115200
-    UCA0MCTL = UCBRS2 + UCBRS0;  // Modulation UCBRSx = 5
-    UCA0CTL1 &= ~UCSWRST;        // **Initialize USCI state machine**
-    UC0IE |= UCA0TXIE;          // Enable USCI_A0 TX interrupt
-    UC0IE &= ~UCA0RXIE;         // Disable RX.
+    UCA0CTL1 |= UCSSEL_2;             // SMCLK
+    UCA0BR0 = 0x08;                   // 1MHz, 115200 baud rate
+    UCA0BR1 = 0x00;                   // 1MHz, 115200 baud rate
+    UCA0MCTL = UCBRS2 + UCBRS0;       // Modulation UCBRSx = 5
+    UCA0CTL1 &= ~UCSWRST;             // USCI state machine initialisation
+    UC0IE |= UCA0TXIE;                // Enable USCI_A0 TX interrupt
+    UC0IE &= ~UCA0RXIE;               // Disable RX
 
     position=117;
-    flag_center=1;
+    flag_center=1;                    // Initialise at centre position
+
     // Transmission Timer Setup
-    TA1CCTL0 |= CCIE;  //Interrupt Enable
+    TA1CCTL0 |= CCIE;                 // Interrupt Enable
     TA1CTL |= TASSEL_1 + MC_1 + ID_3; // Use ACLK (32768 Hz), divide by 8 = 4096, divide by CCR0
     TA1CCR0 = TX_CLOCK;
 
     // Transmission Timer Setup
-    TA1CCTL1 &= ~CCIE;  //Interrupt Disabe - only to be used as a delay to servo rotation
+    TA1CCTL1 &= ~CCIE;                // Interrupt Disable - only to be used as a delay to servo rotation
     TA1CCR1 |= 256; // 0.125s
 
 
-    __delay_cycles(200000);
+    __delay_cycles(200000);           // Delays for setting up the ESP8266.
+
     // Transmission Timer Setup
-    TA1CCTL0=CCIE;  //Interrupt Enable
-    TA1CTL = TASSEL_1 + MC_1 + ID_3; // Use ACLK (32768 Hz), divide by 8 = 4096, divide by CCR0
+    TA1CCTL0=CCIE;                    // Interrupt Enable
+    TA1CTL = TASSEL_1 + MC_1 + ID_3;  // Use ACLK (32768 Hz), divide by 8 = 4096, divide by CCR0
     TA1CCR0 = TX_CLOCK;
     TACCR1 = servo_lut[position];
-    //Center is position 95.
+
     __delay_cycles(20000);
 
-    //Max range is 117 (right side).
-    //Min range is 78. (left side).
+    //Main loop
     while (1) {
-        if(flag_forwards){
+        if(flag_forwards){            // Car moves forwards
             P1OUT |= BIT5;
             TACCR2 |= 0;
             flag_forwards = 0;
         }
-        else if(flag_backwards){
+        else if(flag_backwards){      // Car moves backwards
             P1OUT &= ~BIT5;
             TACCR2 |= 0;
             flag_backwards = 0;
         }
-        if(flag_on){
+        if(flag_on){                  // Car motor is on
             P1SEL |= BIT4;
             P1OUT |= BIT4;
             flag_on=0;
         }
-        else if(flag_off){
+        else if(flag_off){            // Car motor is off
             P1SEL &= ~BIT4;
             P1OUT &= ~BIT4;
             flag_off=0;
         }
-        if(flag_right||flag_left){
-            // Move right toward the maximum step value
-            if(flag_right){
+        if(flag_right||flag_left){    // Change in direction required
+
+            if(flag_right){           // Move right to maximum value
                 position=117;
                 TACCR1 = servo_lut[position];
                 TA1CCTL1 |= CCIE;
                 flag_right = 0;
             }
-            // Move left toward the minimum step value
-            if(flag_left){
+
+            if(flag_left){            // Move left to minimum value
                 position=45;
                 TACCR1 = servo_lut[position];
                 TA1CCTL1 |= CCIE;
@@ -161,11 +170,13 @@ int main(void)
                 previous = 1;
             }
         }
-        if(flag_center){
+        if(flag_center){              // Change to centre
             if(previous){
                 position = 95;
             }
-            else{position=86;}
+            else{
+                position=86;
+            }
 
             TACCR1 = servo_lut[position];
             TA1CCTL1 |= CCIE;
@@ -173,37 +184,38 @@ int main(void)
             previous=0;
         }
         TA1CCTL1 |= CCIE;
-        __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+        __bis_SR_register(CPUOFF + GIE); // Enter low power mode
     }
 }
 
+//Interrupt service routine for UART transmission
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void)
 {
-    if (index>=size){       //if whole message has been sent
-        P1OUT &= ~TXLED;    //Turn off transmitter LED
-        UC0IE &= ~UCA0TXIE; // Disable transmitter interrupts
-        UC0IE |= UCA0RXIE; //Enable receiver interrupts
+    if (index>=size){                   // Check if all instructions have sent
+        P1OUT &= ~TXLED;                // Turn off transmitter LED
+        UC0IE &= ~UCA0TXIE;             // Disable transmitter interrupt
+        UC0IE |= UCA0RXIE;              // Enable receiver interrupt
     }
-    else{       //more commands to send
+    else{
         temp[i] = AP[index][i];
-        UCA0TXBUF = AP[index][i++]; // iterate next character
-        if (i == strlen(AP[index])+1){ // message is over
-            index++;    //go to next message
+        UCA0TXBUF = AP[index][i++];     // Send one character with TX buffer
+        if (i == strlen(AP[index])+1){  // Check for end of single instruction
+            index++;
             i=0;
-            UC0IE &= ~UCA0TXIE; // Disable transmitter interrupts
-            TA1CCTL0 = CCIE;              // Timer 1 interrupt enable
+            UC0IE &= ~UCA0TXIE;         // Disable transmitter interrupt
+            TA1CCTL0 = CCIE;            // Enable timer 1 interrupt
             memset(&temp[0], 0, sizeof(temp));
         }
     }
 }
-
+//Interrupt service routine for timer
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void timer(void)
 {
         TA1CCTL0 &= ~CCIE;            // Timer 1 interrupt disable
         P1OUT |= TXLED;
-        UC0IE |= UCA0TXIE;          // Enable transmitter interrupts
+        UC0IE |= UCA0TXIE;             // Enable transmitter interrupts
 }
 
 #pragma vector=TIMER1_A1_VECTOR
@@ -212,38 +224,44 @@ __interrupt void timerServo(void)
         TA1CCTL1 &= ~CCIE;            // Timer 1 interrupt disable
 }
 
+//Interrupt service routine for UART receiver
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-    //On.
+    //Motor is on
     if(UCA0RXBUF=='7'){
         flag_on = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE); // Exit low power mode
     }
-    //Off.
+    //Motor is off
     if(UCA0RXBUF=='9'){
         flag_off = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE);
     }
+    //Turn right
     if(UCA0RXBUF=='6'){
         flag_right = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE);
     }
+    //Turn left
     if(UCA0RXBUF=='4'){
         flag_left = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE);
     }
+    //Turn to centre
     if(UCA0RXBUF=='5'){
         flag_center = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE);
     }
+    //Move forwards
     if(UCA0RXBUF=='8'){
         flag_forwards = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
-    }
+        __bic_SR_register_on_exit(CPUOFF+GIE);
+
+    //Move backwards
     if(UCA0RXBUF=='2'){
         flag_backwards = 1;
-        __bic_SR_register_on_exit(CPUOFF+GIE); // Enter LPM0 w/ int until Byte RXed
+        __bic_SR_register_on_exit(CPUOFF+GIE);
     }
 }
 
@@ -251,11 +269,11 @@ __interrupt void USCI0RX_ISR(void)
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-  //turn the engine on and off
+  //Turn the engine on and off
   P1SEL ^= BIT4;
   P1OUT ^= BIT4;
 
-  //toggle direction
+  //Toggle direction
   P1OUT ^= BIT5;                            // P1.0 = toggle
   P1IFG &= ~BIT3;                           // P1.3 IFG cleared
 }
