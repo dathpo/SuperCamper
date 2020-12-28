21643eb04d7a31edca3f16610519cddb97d44090
David T. Pocock <davidtpocock@gmail.com>
2020-12-28 00:34:19 +0000

    Modify .gitignore, delete sample tests and clean up project

--- App/app/src/main/java/com/example/group3/supercamper/MainActivity.java
+++ App/app/src/main/java/com/example/group3/supercamper/MainActivity.java
@@ -5,26 +5,23 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private TcpClient mTcpClient = null;
     private ConnectTask connecttask = null;
     private String ipAddressOfServerDevice;
-    private String lastMessage="";
-    ImageButton b1,b2;
-    Switch s1,s2;
-    boolean flag_left,flag_right,flag_down,flag_up,flag_on,flag_off,flag_center;
+    private String lastMessage = "";
+    ImageButton b1, b2;
+    Switch s1, s2;
+    boolean flag_left, flag_right, flag_down, flag_up, flag_on, flag_off, flag_center;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +29,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final MediaPlayer horn = MediaPlayer.create(MainActivity.this, R.raw.car_horn);
         final MediaPlayer start = MediaPlayer.create(MainActivity.this, R.raw.start);
-
-
-
         final MediaPlayer turning = MediaPlayer.create(MainActivity.this, R.raw.turning);
         final MediaPlayer forward = MediaPlayer.create(MainActivity.this, R.raw.forward);
         final MediaPlayer reverse = MediaPlayer.create(MainActivity.this, R.raw.reverse);
         final MediaPlayer left = MediaPlayer.create(MainActivity.this, R.raw.left);
         final MediaPlayer engineOff = MediaPlayer.create(MainActivity.this, R.raw.engine_off);
-
-
         horn.start();
         forward.setLooping(true);
         reverse.setLooping(true);
         engineOff.setLooping(true);
-
         ipAddressOfServerDevice = "192.168.4.1";
         connecttask = new ConnectTask();
         connecttask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-
         b1 = findViewById(R.id.leftArrow);
         b2 = findViewById(R.id.rightArrow);
         s1 = findViewById(R.id.engineSwitch);
         s2 = findViewById(R.id.directionSwitch);
-
         b1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -64,13 +53,11 @@ public class MainActivity extends AppCompatActivity {
                     flag_left = true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     flag_left = false;
-                    flag_center=true;
-
+                    flag_center = true;
                 }
                 return false;
             }
         });
-
         b2.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent event) {
@@ -79,14 +66,12 @@ public class MainActivity extends AppCompatActivity {
                     flag_right = true;
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     flag_right = false;
-                    flag_center=true;
+                    flag_center = true;
                 }
                 return false;
             }
         });
-
-
-        s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+        s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton cb, boolean on) {
                 if (on) {
@@ -97,8 +82,7 @@ public class MainActivity extends AppCompatActivity {
                         forward.start();
                     } else
                         reverse.start();
-                }
-                else {
+                } else {
                     flag_off = true;
                     forward.stop();
                     reverse.stop();
@@ -108,23 +92,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        s2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+        s2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton cb, boolean on) {
                 if (on) {
                     flag_up = true;
                     s2.setText("Forward");
-                    if (s1.isChecked()){
+                    if (s1.isChecked()) {
                         forward.start();
                         reverse.stop();
                         reverse.prepareAsync();
                     }
-                }
-                else {
+                } else {
                     flag_down = true;
                     s2.setText("Reverse");
-                    if (s1.isChecked()){
+                    if (s1.isChecked()) {
                         reverse.start();
                         forward.stop();
                         forward.prepareAsync();
@@ -132,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         handler.post(r);
     }
 
@@ -140,40 +121,40 @@ public class MainActivity extends AppCompatActivity {
     Runnable r = new Runnable() {
         @Override
         public void run() {
-            if(flag_left){
+            if (flag_left) {
                 sendMessage("4");
-                flag_left=false;
-            } else if(flag_right){
+                flag_left = false;
+            } else if (flag_right) {
                 sendMessage("6");
-                flag_right=false;
-            } else if(flag_down) {
+                flag_right = false;
+            } else if (flag_down) {
                 sendMessage("2");
-                flag_down=false;
-            } else if(flag_up) {
+                flag_down = false;
+            } else if (flag_up) {
                 sendMessage("8");
-                flag_up=false;
-            } else if(flag_on){
+                flag_up = false;
+            } else if (flag_on) {
                 sendMessage("7");
-                flag_on=false;
-            }else if(flag_off){
+                flag_on = false;
+            } else if (flag_off) {
                 sendMessage("9");
-                flag_off=false;
-            }
-            else if(flag_center){
+                flag_off = false;
+            } else if (flag_center) {
                 sendMessage("5");
-                flag_center=false;
+                flag_center = false;
             }
-            handler.postDelayed(r,100);
+            handler.postDelayed(r, 100);
         }
     };
 
-    private void sendMessage(String s){
+    private void sendMessage(String s) {
         if (!s.equals(lastMessage)) {
             lastMessage = s;
             System.out.println(s);
             mTcpClient.sendMessage(s);
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -187,12 +168,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -200,7 +179,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected TcpClient doInBackground(String... message) {
-
             mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
                 @Override
                 public void messageReceived(String message) {
@@ -223,16 +201,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy()
-    {
-        try{
+    protected void onDestroy() {
+        try {
             System.out.println("onDestroy.");
             mTcpClient.sendMessage("bye");
             mTcpClient.stopClient();
             connecttask.cancel(true);
             connecttask = null;
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         super.onDestroy();
--- App/app/src/main/java/com/example/group3/supercamper/TcpClient.java
+++ App/app/src/main/java/com/example/group3/supercamper/TcpClient.java
@@ -1,23 +1,19 @@
 package com.example.group3.supercamper;
+
 import android.util.Log;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import android.util.Log;
 
 /**
  * Created by aidanhowie on 07/02/18.
  */
-
 public class TcpClient {
 
     private String serverMessage;
     private String messageToSend;
-    public String serverIp =  "192.168.4.1"; //server IP address
+    public String serverIp = "192.168.4.1"; //server IP address
     public static final int SERVER_PORT = 100;
     // sends message received notifications
     private OnMessageReceived mMessageListener = null;
@@ -30,8 +26,7 @@ public class TcpClient {
     /**
      * Constructor of the class. OnMessagedReceived listens for the messages received from server
      */
-    public TcpClient(final OnMessageReceived listener, String ipAddressOfServerDevice)
-    {
+    public TcpClient(final OnMessageReceived listener, String ipAddressOfServerDevice) {
         mMessageListener = listener;
         serverIp = ipAddressOfServerDevice;
     }
@@ -54,74 +49,50 @@ public class TcpClient {
         };
         Thread thread = new Thread(runnable);
         thread.start();
-
     }
 
     /**
      * Close the connection and release the members
      */
     public void stopClient() {
-
         mRun = false;
     }
 
     public void run() {
-
         mRun = true;
-
         try {
             //here you must put your computer's IP address.
             InetAddress serverAddr = InetAddress.getByName(serverIp);
-
             Log.e("TCP SI Client", "SI: Connecting...");
-
             //create a socket to make the connection with the server
             Socket socket = new Socket(serverAddr, SERVER_PORT);
-
             try {
-
                 //sends the message to the server
                 out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
                 Log.e("TCP SI Client", "SI: Sent.");
-
                 Log.e("TCP SI Client", "SI: Done.");
-
-
                 //receives the message which the server sends back
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-
                 //in this while the client listens for the messages sent by the server
                 while (mRun) {
-
                     serverMessage = in.readLine();
-
                     if (serverMessage != null && mMessageListener != null) {
                         //call the method messageReceived from MyActivity class
                         mMessageListener.messageReceived(serverMessage);
                         Log.e("RESPONSE FROM SERVER", "S: Received Message: " + serverMessage + "'");
                     }
                     serverMessage = null;
-
                 }
-
             } catch (Exception e) {
-
                 Log.e("TCP SI Error", "SI: Error", e);
-
             } finally {
                 //the socket must be closed. It is not possible to reconnect to this socket
                 // after it is closed, which means a new socket instance has to be created.
                 socket.close();
             }
-
         } catch (Exception e) {
-
             Log.e("TCP SI Error", "SI: Error", e);
-
         }
-
     }
 
     //Declare the interface. The method messageReceived(String message) will must be implemented in the MyActivity
@@ -129,5 +100,4 @@ public class TcpClient {
     public interface OnMessageReceived {
         public void messageReceived(String message);
     }
-
 }
